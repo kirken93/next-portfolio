a4a00a7ccba6b4489cdc570cedfced963e7d4199
Megan Kirkbride <megan.kirkbride@bloomerang.co>
2021-03-01 19:41:54 -0500

    WIP on rewrite

---
+++ components/Card.js
@@ -0,0 +1,5 @@
+export default function Card(props) {
+  return <div className="surface">
+    {props.children}
+  </div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,18 @@
-import Head from "next/head";
-
-import Nav from "@components/Nav";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
+import Card from "@components/Card";
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Megan Kirkbride</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"></link>
-      </Head>
-      <Nav />
-
-      <main>
-        <Header text="Megan Kirkbride" />
-        <img
-          src="/painted_rocks.jpeg"
-          className="painted-rocks"
-          alt="Megan at the painted rocks"
-          height="400"
-        />
-        <div className="about-content">Hello! My name is Megan Kirkbride</div>
-      </main>
-
-      <Footer />
+    <div className="surface">
+      <h2>Welcome!</h2>
+      <img
+        src="/painted_rocks.jpeg"
+        className="painted-rocks"
+        alt="Megan at the painted rocks"
+        height="400"
+      />
+      <div className="about-content">
+        Hello! My name is Megan Kirkbride and I'm a software engineer.
+      </div>
     </div>
   );
 }
